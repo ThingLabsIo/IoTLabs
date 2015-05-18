9fa3e86fc2edebda6041c7a84921f79fa08c2a4c
dougseven <doug@dougseven.com>
2015-05-18 15:40:41 -0700

    Update lab005.js

--- Lab005/lab005.js
+++ Lab005/lab005.js
@@ -7,8 +7,6 @@
   command is processed. In this lab you are simulating two devices - an
   ambient light sensor and a light -- with a single Arduino.
 */
-var verboseLogging = false;
-
 var five = require ("johnny-five"),
     board, photoresistor,led;
     
@@ -41,27 +39,31 @@ lightSensor = new nitrogen.Device({
 // to the Nitrogen service instance.
 service.connect(lightSensor, function(err, session, lightSensor) {
     if (err) { return console.log('Failed to connect lightSensor: ' + err); }
-    // Create a new CommandManager object for the light (on/off)
+    
+    // Create an instance of the subclassed CommandManager object for the lightSensor
     new LightManager(lightSensor).start(session, function(err, message) { 
         if (err) return session.log.error(JSON.stringify(err)); 
     });
-
+    
     board.on("ready", function() {
         console.log("Board connected...");
+    
         // Create a new `photoresistor` hardware instance.
         photoresistor = new five.Sensor({
             pin: 'A0',  // Analog pin 0
             freq: 1000  // Collect data once per second
         });
+    
         // Define the LED object using the pin
         led = new five.Led(LEDPIN);
+   
         // Inject the `sensor` hardware into the Repl instance's context;
         // Allows direct command line access
         board.repl.inject({
             pot: photoresistor,
             led:led
         });
-
+        
         // Define the event handler for the photo resistor reading
         // The freq value used when the photoresistor was defined
         // determines how often this is invoked, thus controlling
@@ -69,6 +71,7 @@ service.connect(lightSensor, function(err, session, lightSensor) {
         photoresistor.on('data', function() {
             // Capture the ambient light level from the photo resistor
             var lightLevel = this.value;
+            
             // Create a Nitrogen Message to send the _lightLevel
             var ambientLightMessage = new nitrogen.Message({
                 type: '_lightLevel',
@@ -80,6 +83,7 @@ service.connect(lightSensor, function(err, session, lightSensor) {
                 },
                 to: lightSensor.id
             });
+            
             // Send the message
             ambientLightMessage.send(session);
             
@@ -132,18 +136,22 @@ LightManager.prototype.obsoletes = function(downstreamMsg, upstreamMsg) {
 // Executes the active commands in the message queue
 LightManager.prototype.executeQueue = function(callback) {
     if (!this.device) return callback(new Error('no device attached to control manager.'));
+
     var self = this;
+
     // CommandManager.activeCommands()
     // Returns the array of commands that are currently active for this manager
     var activeCommands = this.activeCommands();
+
     // Verify there are activeCommands
     if (activeCommands.length === 0) {
         this.session.log.warn('LightManager::executeQueue: no active commands to execute.');
         return callback();
     }
-
+    
     var commandIds = [];
     var lightOn;
+
     // Find the final state and collect all the active command ids
     // You will use them in a moment.
     activeCommands.forEach(function(activeCommand) {
@@ -153,22 +161,21 @@ LightManager.prototype.executeQueue = function(callback) {
         var light = activeCommand.body.command.ambientLight;
         
         // Determine the final state of the light (on/true or off/false)
-        if (light > 350) {
-            lightOn = true;
-        } else {
-            lightOn = false;
-        }
+        lightOn = light > 350;
     });
     
     // Turn the light on or off based on final state
-    if (led != null) {
+    if (led != null) { // Make sure the led sensor is available before using it
         if(lightOn) { 
            led.on();
         } else { 
            led.off();
         }
     }
-
+    else {
+        console.log("ERROR: 'led' was null.");
+    }
+    
     // This is the response to the _lightLevel command.
     // Notice the response_to is the array of command ids from above. This is used in the obsoletes method above as well.
     var lightMessage = new nitrogen.Message({
@@ -181,13 +188,13 @@ LightManager.prototype.executeQueue = function(callback) {
         },
         response_to: commandIds
     });
-
+    
     lightMessage.send(this.session, function(err, message) {
         if (err) return callback(err);
         
         console.log("Message sent: " + JSON.stringify(lightMessage));
         
-        // let the command manager know we processed this _lightState message by passing it the _isOn message.
+        // let the command manager know we processed this _lightState message.
         self.process(new nitrogen.Message(lightMessage));
 
         // need to callback if there aren't any issues so commandManager can proceed.   

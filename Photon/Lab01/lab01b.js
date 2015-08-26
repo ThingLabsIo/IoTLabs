47d5efe6227430bf56c6d42491457799a47a9441
Doug Seven <doug@dougseven.com>
2015-08-25 23:57:17 -0700

    Photon lab updates

---
+++ Photon/Lab01/lab01b.js
@@ -0,0 +1,51 @@
+/*
+Particle Photon 'Hello World' with Node.js and Johnny-Five
+By: Doug Seven
+Licence: MIT
+
+This example is based on the documentation for the Spark-IO
+plug-in for Johnny Five by Rick Waldron <waldron.rick@gmail.com>.
+See https://github.com/rwaldron/spark-io for the complete
+documentation. 
+*/
+
+// Define the Jonny Five and Spark-IO variables
+var five = require("johnny-five");
+var spark = require("spark-io");
+// Define the Johnny Five board as your Particle Photon
+var board1 = new five.Board({
+  io: new spark({
+    token: process.env.PARTICLE_KEY || 'YOUR API KEY HERE',
+    deviceId: 'D7P003'
+  })
+});
+
+var board2 = new five.Board({
+  io: new spark({
+    token: process.env.PARTICLE_KEY || 'YOUR API KEY HERE',
+    deviceId: 'D7P004'
+  })
+});
+// Define the pin that is connected to the LED
+var LEDPIN = "D7";
+// THe board.on() executes the anonymous function when the 
+// Partile Photon reports back that it is initialized and ready.
+board1.on("ready", function(){
+  // Set the pin you connected to the LED to OUTPUT mode
+  this.pinMode(LEDPIN, five.Pin.OUTPUT);
+  // Create a loop to "flash/blink/strobe" an led
+  var val = 0;
+  this.loop( 1000, function() {
+    this.digitalWrite(LEDPIN, (val = val ? 0 : 1));
+  });
+});
+
+board2.on("ready", function(){
+  // Set the pin you connected to the LED to OUTPUT mode
+  this.pinMode(LEDPIN, five.Pin.OUTPUT);
+  // Create a loop to "flash/blink/strobe" an led
+  var val = 0;
+  this.loop( 500, function() {
+    this.digitalWrite(LEDPIN, (val = val ? 0 : 1));
+  });
+});
---
+++ Photon/Lab04/lab04.js
@@ -0,0 +1,86 @@
+/*
+Particle Photon 'COnnected Ambient Light' with Node.js and Johnny-Five
+By: Doug Seven
+Licence: MIT
+
+This example uses Nitrogen, an open source IoT service.
+Read more about Nitrogen at http://nitrogen.io 
+*/
+
+// Define the Jonny Five and Spark-IO variables
+var five = require("johnny-five"),
+    board, photoresistor;
+var Spark = require("spark-io");
+var Store = require("nitrogen-file-store"),
+    nitrogen = require("nitrogen"),
+    service, lightSensor;
+    
+var config = {
+    host: process.env.HOST_NAME || 'api.nitrogen.io',
+    http_port: process.env.PORT || 443,
+    protocol: process.env.PROTOCOL || 'https',
+    api_key: process.env.NITROGEN_KEY || 'YOUR API KEY HERE'
+};
+
+config.store = new Store(config);
+service = new nitrogen.Service(config);
+
+lightSensor = new nitrogen.Device({
+    nickname: process.env.PARTICLE_DEVICE + 'lab04_lightSensor',
+    name: process.env.PARTICLE_DEVICE + 'Lab 04 Light Sensor'
+});
+    
+// Define the Johnny Five board as your Particle Photon
+board = new five.Board({
+  io: new Spark({
+    token: process.env.PARTICLE_KEY || 'YOUR API KEY HERE',
+    deviceId: process.env.PARTICLE_DEVICE || 'YOUR DEVICE ID OR ALIAS HERE'
+  })
+});
+
+// Connect the lightSensor device defined above
+// to the Nitrogen service instance.
+service.connect(lightSensor, function(err, session, lightSensor) {
+    if (err) { return console.log('Failed to connect lightSensor: ' + err); }
+
+    // The board.on() executes the anonymous function when the 
+    // Partile Photon reports back that it is initialized and ready.
+    board.on("ready", function(){
+      console.log("Board connected...");
+        
+        // Create a new 'photoresistor' hardware instance.
+        photoresistor = new five.Sensor({
+            pin: "A0",  // Analog pin 0
+            freq: 1000  // Collect data once per second
+        });
+    
+        // Inject the 'sensor' hardware into the Repl instance's context;
+        // Allows direct command line access
+        board.repl.inject({
+            pot: photoresistor
+        });
+
+        // Define the callback function for the photoresistor reading
+        // The freq value used when the photoresistor was defined
+        // determines how often this is invoked, thus controlling
+        // the frequency of Nitrogen messages.
+        photoresistor.on('data', function() {
+            // Capture the ambient light level from the photoresistor
+            var lightLevel = this.value;
+
+            // Create a Nitrogen message
+            var message = new nitrogen.Message({
+                type: '_lightLevel',
+                body: {
+                    ambientLight: lightLevel
+                }
+            });
+            
+            // Log the light level value for debugging    
+            session.log.info('Sending ambientLight: ' + lightLevel);
+
+            // Send the message
+            message.send(session);
+        });
+    });
+});

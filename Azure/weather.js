17f94b5d7a6ac41a804a5a314e76ce6c476780ce
dougseven <doug@dougseven.com>
2015-11-20 18:48:37 -0800

    Updated to match tutorial

--- Azure/weather.js
+++ Azure/weather.js
@@ -1,45 +1,58 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license.
 'use strict';
-// Define the Johnny Five, Particle and Azure IoT objects
+// Define the objects you will be working with
 var five = require ("johnny-five");
-var Weather = require("j5-sparkfun-weather-shield")(five);
+var Shield = require("j5-sparkfun-weather-shield")(five);
 var device = require('azure-iot-device');
+
+/* 
+// PARTICLE PHOTON USERS
+// Add the following definition for the Particle plugin for Johnny-Five
 var Particle = require("particle-io");
 
-// Set up the access credentials for Particle and Azure
-var token = process.env.PARTICLE_KEY || 'YOUR PARTICLE ACCESS TOKEN HERE';
-var deviceId = process.env.DEVICE_NAME || 'YOUR PARTICLE PHOTON DEVICE ID/ALIAS HERE';
-var location = process.env.DEVICE_LOCATION || 'THE LOCATION OF THE PARTICLE PHOTON DEVICE';
-var connectionString = process.env.IOTHUB_CONN || 'YOUR IOT HUB DEVICE-SPECIFIC CONNECTION STRING HERE';
+var token = 'YOUR PARTICLE ACCESS TOKEN HERE';
+*/
 
-// Create a Johnny-Five board instance to represent your Particle Photon
-// Board is simply an abstraction of the physical hardware, whether is is a 
-// Photon, Arduino, Raspberry Pi or other boards.
-var board = new five.Board({
-  io: new Particle({
-    token: token,
-    deviceId: deviceId
-  })
-});
+var location = process.env.DEVICE_LOCATION || 'GIVE A NAME TO THE LOCATION OF THE THING';
+var connectionString = process.env.IOTHUB_CONN || 'YOUR IOT HUB DEVICE-SPECIFIC CONNECTION STRING HERE';
 
 // Create an Azure IoT client that will manage the connection to your IoT Hub
 // The client is created in the context of an Azure IoT device, which is why
 // you use a device-specific connection string.
-var client = new device.Client(connectionString, new device.Https());
+var client = device.Client.fromConnectionString(connectionString);
+var deviceId = client.ConnectionString.parse(connectionString).DeviceId;
+
+// Create a Johnny-Five board instance to represent your Particle Photon
+// Board is simply an abstraction of the physical hardware, whether is is a 
+// Photon, Arduino, Raspberry Pi or other boards.
+var board = new five.Board();
+/* 
+// PARTICLE PHOTON USERS
+// When creating a Board instance for the Photon you must specify the token and device ID
+// for your Photon using the Particle-IO Plugin for Johnny-five.
+// Replace the Board instantiation with the following:
+var board = new five.Board({
+  io: new Particle({
+    token: token,
+    deviceId: 'YOUR PARTICLE PHOTON DEVICE IS OR ALIAS'
+  })
+});
+*/
 
 // The board.on() executes the anonymous function when the 
 // board reports back that it is initialized and ready.
 board.on("ready", function() {
     console.log("Board connected...");
-    // The SparkFun Weather Shield for the Particle Photon has two sensors on the I2C bus - 
+    
+    // The SparkFun Weather Shield has two sensors on the I2C bus - 
     // a humidity sensor (HTU21D) which can provide both humidity and temperature, and a 
     // barometer (MPL3115A2) which can provide both barometric pressure and humidity.
-    // Controllers for these are wrapped in the convenient `Weather` plugin class:
-    var weather = new Weather({
-      variant: "PHOTON",
-      freq: 1000,
-      elevation: -1
+    // Controllers for these are wrapped in a convenient plugin class:
+    var weather = new Shield({
+      variant: "ARDUINO", // or PHOTON
+      freq: 1000,         // Set the callback frequency to 1-second
+      elevation: 100      // Go to http://www.WhatIsMyElevation.com to get your current elevation
     });
     
     // The weather.on("data", callback) function invokes the anonymous callback function 
@@ -49,7 +62,7 @@ board.on("ready", function() {
       var payload = JSON.stringify({
         deviceId: deviceId,
         location: location,
-        // celsius & fahrenheit are averages taken from both sensors
+        // celsius & fahrenheit are averages taken from both sensors on the shield
         celsius: this.celsius,
         fahrenheit: this.fahrenheit,
         relativeHumidity: this.relativeHumidity,
@@ -60,8 +73,10 @@ board.on("ready", function() {
       
       // Create the message based on the payload JSON
       var message = new device.Message(payload);
+      
       // For debugging purposes, write out the message payload to the console
       console.log("Sending message: " + message.getData());
+      
       // Send the message to Azure IoT Hub
       client.sendEvent(message, printResultFor('send'));
     });

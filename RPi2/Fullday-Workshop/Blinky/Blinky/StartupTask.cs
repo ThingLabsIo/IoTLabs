395b98656e227dd31febc5e14796b9d393c9eb05
Doug Seven <doug@dougseven.com>
2016-03-23 22:28:18 -0700

    Blinky Example

---
+++ RPi2/Fullday-Workshop/Blinky/Blinky/StartupTask.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using Windows.ApplicationModel.Background;
+
+using GrovePi;
+using GrovePi.Sensors;
+using Windows.System.Threading;
+
+namespace Blinky
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        ThreadPoolTimer timer;
+        BackgroundTaskDeferral deferral;
+        ILed led;
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            deferral = taskInstance.GetDeferral();
+
+            // Connect the LED to digital port 4
+            led = DeviceFactory.Build.Led(Pin.DigitalPin4);
+
+            // Create a timer that will 'tick' every one-second
+            timer = ThreadPoolTimer.CreatePeriodicTimer(this.Timer_Tick, TimeSpan.FromSeconds(1));
+        }
+
+        private void Timer_Tick(ThreadPoolTimer timer)
+        {
+            if (led.CurrentState == SensorStatus.Off)
+            {
+                led.ChangeState(SensorStatus.On);
+            }
+            else {
+                led.ChangeState(SensorStatus.Off);
+            }
+        }
+    }
+}
